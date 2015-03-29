e3ae398a3319b371b9199fd494fd9f55e92df10f
Roger Goldfinger <rgoldfinger@gmail.com>
2015-03-29 00:27:01 -0700

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,12 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loader: 'jsx-loader?harmony,insertPragma=React.DOM' },
       { test: /\.jsx$/, loader: 'jsx-loader?harmony,insertPragma=React.DOM' },
-      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }, // use ! to chain loaders
+      { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' }, // use ! to chain loaders
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'} // inline base64 URLs for <=8k images, direct URLs for the rest
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx', 'styl']
   }
 };
